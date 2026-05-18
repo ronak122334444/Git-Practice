ae1e3c21de101ee66bf4738b60e795e6f7238f20
Deepjyot Singh <deepjyotsingh49@gmail.com>
2026-05-18 19:11:15 +0530

    Added Calculator program

---
+++ calculator.java
@@ -0,0 +1,14 @@
+import java.rmi.server.SocketSecurityException;
+
+public class calculator {
+    public static void main (String[] args) {
+        int a =15;
+        int b=5;
+        System.out.println("Addition = " + (a+b));
+        System.out.println("Subtraction = " + (a-b));
+        System.out.println("Multiplication = " + (a*b));
+        System.out.println("Division = " + (a/b));
+      
+    }
+    
+}
