0279df5e40dd7e8ecdc188898bda06edff94e837
Deepjyot Singh <deepjyotsingh49@gmail.com>
2026-05-18 18:49:31 +0530

    Updated Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
       
-        System.out.println("Hello Deepjyot");
+        System.out.println("Hello java Developer");
     }
 }
